7f8588fefac987b06edd2e1faa25973f21534232
Kishor-Kumar-Roy <165455986+Kishor-Kumar-Roy@users.noreply.github.com>
2024-03-31 10:42:38 +0530

    Add files via upload

---
+++ larger number using ternary operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2;
+printf("enter value of num1 and num2: ");
+scanf("%d%d",&num1,&num2);
+num1>num2? printf("%d is larger number",num1):printf("%d is larger number",num2);
+return 0;
+}
